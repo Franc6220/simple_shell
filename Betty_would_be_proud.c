28f3df90c19dce8c1821ea6e33c189ba626863cb
Franc6220 <samuelwangalibo@yahoo.com>
2023-11-30 01:31:27 -0800

    Betty would be proud

--- Betty_would_be_proud.c
+++ Betty_would_be_proud.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Description: Program to illustrate operators in C
@@ -11,10 +12,15 @@ int main(void)
 int a = 20, b = 15, c, d, e = 10;
 c = a++;
 d = a;
+pid_t pid;
+
+pid = getpid();
+
 printf("%d\n", a < b > c == d);
 printf("%d\n", a == c);
 printf("%d\n", !(d < e));
 printf("%d\n", e > d && c <= a);
 printf("%d\n", a);
+printf("pid is %u\n", pid);
 return (0);
 }
