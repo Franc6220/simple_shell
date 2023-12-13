f206eb402ed75aaf8d44e96bf6a0649146ae52df
Franc6220 <samuelwangalibo@yahoo.com>
2023-12-12 23:03:33 -0800

    Betty

--- Betty_would_be_proud.c
+++ Betty_would_be_proud.c
@@ -11,12 +11,10 @@ int main(void)
 int a = 20, b = 15, c, d, e = 10;
 c = a++;
 d = a;
-
 printf("%d\n", a < b > c == d);
 printf("%d\n", a == c);
 printf("%d\n", !(d < e));
 printf("%d\n", e > d && c <= a);
 printf("%d\n", a);
-
 return (0);
 }
