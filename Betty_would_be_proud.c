3bec798d955f91ff66dd2c29489d51eb1f35fab1
Franc6220 <samuelwangalibo@yahoo.com>
2023-12-12 22:55:14 -0800

    Betty

--- Betty_would_be_proud.c
+++ Betty_would_be_proud.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * Description: Program to illustrate operators in C
- * ppid: get parent process ID
+ * 
  *
  * Return: Always 0
  */
@@ -12,15 +12,12 @@ int main(void)
 int a = 20, b = 15, c, d, e = 10;
 c = a++;
 d = a;
-pid_t ppid;
-
-ppid = getppid();
 
 printf("%d\n", a < b > c == d);
 printf("%d\n", a == c);
 printf("%d\n", !(d < e));
 printf("%d\n", e > d && c <= a);
 printf("%d\n", a);
-printf("ppid is %u\n", ppid);
+
 return (0);
 }
