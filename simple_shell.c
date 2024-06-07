586a12521e50c0f81454697418277d1ef525ec90
Franc6220 <samuelwangalibo@yahoo.com>
2024-06-07 06:41:19 -0700

    shell.0.2

--- simple_shell.c
+++ simple_shell.c
@@ -25,41 +25,41 @@ void print_env(void);
  */
 int main(void)
 {
-	char *line;
-	char **args;
-	int status = 1;
+    char *line;
+    char **args;
+    int status = 1;
 
-	while (status)
-	{
-		display_prompt();
-		line = read_line();
-		if (line == NULL) /* Handle EOF (Ctrl+D) */
-		{
-			printf("\n");
-			break;
-		}
-		args = split_line(line);
-		if (args[0] != NULL)
-		{
-			if (strcmp(args[0], "exit") == 0) /* Handle exit command */
-			{
-				free(line);
-				free(args);
-				exit(0);
-			}
-			else if (strcmp(args[0], "env") == 0) /* Handle env command */
-			{
-				print_env();
-			}
-			else
-			{
-				execute_command(args);
-			}
-		}
-		free(line);
-		free(args);
-	}
-	return (0);
+    while (status)
+    {
+        display_prompt();
+        line = read_line();
+        if (line == NULL) /* Handle EOF (Ctrl+D) */
+        {
+            printf("\n");
+            break;
+        }
+        args = split_line(line);
+        if (args[0] != NULL)
+        {
+            if (strcmp(args[0], "exit") == 0) /* Handle exit command */
+            {
+                free(line);
+                free(args);
+                exit(0);
+            }
+            else if (strcmp(args[0], "env") == 0) /* Handle env command */
+            {
+                print_env();
+            }
+            else
+            {
+                execute_command(args);
+            }
+        }
+        free(line);
+        free(args);
+    }
+    return (0);
 }
 
 /**
@@ -67,8 +67,8 @@ int main(void)
  */
 void display_prompt(void)
 {
-	printf("%s", PROMPT);
-	fflush(stdout); /* Ensure prompt is displayed immediately */
+    printf("%s", PROMPT);
+    fflush(stdout); /* Ensure prompt is displayed immediately */
 }
 
 /**
@@ -78,34 +78,32 @@ void display_prompt(void)
  */
 char *read_line(void)
 {
-	char *line = NULL;
-	size_t bufsize = 0; /* size_t for getline */
-	size_t len;
+    char *line = NULL;
+    size_t bufsize = 0; /* size_t for getline */
+    size_t len;
+    ssize_t chars_read;
 
-	if (getline(&line, &bufsize, stdin) == -1)
-	{
-		if (feof(stdin))  /* End of file (Ctrl+D) */
-		{
-			free(line);
-			return (NULL);
-		}
-		else
-		{
-			perror("read_line");
-			exit(EXIT_FAILURE);
-		}
-	}
+    chars_read = getline(&line, &bufsize, stdin);
+    if (chars_read == -1)
+    {
+        if (feof(stdin))  /* End of file (Ctrl+D) */
+        {
+            free(line);
+            return (NULL);
+        }
+        else
+        {
+            perror("read_line");
+            exit(EXIT_FAILURE);
+        }
+    }
 
-	/* Remove the prompt characters */
-	if (strstr(line, PROMPT) == line)
-		line += strlen(PROMPT);
+    /* Remove newline character if present */
+    len = strlen(line);
+    if (len > 0 && line[len - 1] == '\n')
+        line[len - 1] = '\0';
 
-	/* Remove newline character if present */
-	len = strlen(line);
-	if (len > 0 && line[len - 1] == '\n')
-		line[len - 1] = '\0';
-
-	return (line);
+    return (line);
 }
 
 /**
@@ -116,36 +114,49 @@ char *read_line(void)
  */
 char **split_line(char *line)
 {
-	int bufsize = MAX_INPUT, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
-	char *token;
+    int bufsize = MAX_INPUT, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token, *copy, *saveptr;
 
-	if (!tokens)
-	{
-		fprintf(stderr, "simple_shell: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
+    if (!tokens)
+    {
+        fprintf(stderr, "simple_shell: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
 
-	token = strtok(line, " \t\r\n\a");
-	while (token != NULL)
-	{
-		tokens[position] = token;
-		position++;
+    copy = strdup(line);
+    if (!copy)
+    {
+        perror("split_line");
+        exit(EXIT_FAILURE);
+    }
 
-		if (position >= bufsize)
-		{
-			bufsize += MAX_INPUT;
-			tokens = realloc(tokens, bufsize * sizeof(char *));
-			if (!tokens)
-			{
-				fprintf(stderr, "simple_shell: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-		token = strtok(NULL, " \t\r\n\a");
-	}
-	tokens[position] = NULL;
-	return (tokens);
+    token = strtok_r(copy, " \t\r\n\a", &saveptr);
+    while (token != NULL)
+    {
+        tokens[position] = strdup(token);
+        if (!tokens[position])
+        {
+            perror("split_line");
+            exit(EXIT_FAILURE);
+        }
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += MAX_INPUT;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)
+            {
+                perror("split_line");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok_r(NULL, " \t\r\n\a", &saveptr);
+    }
+    tokens[position] = NULL;
+    free(copy);
+    return (tokens);
 }
 
 /**
@@ -154,42 +165,42 @@ char **split_line(char *line)
  */
 void execute_command(char **args)
 {
-	pid_t pid;
-	int status;
-	char *command_path;
+    pid_t pid;
+    int status;
+    char *command_path;
 
-	command_path = find_command_in_path(args[0]);
-	if (command_path == NULL)
-	{
-		/* Command not found */
-		fprintf(stderr, "simple_shell: %s: command not found\n", args[0]);
-		return;
-	}
+    command_path = find_command_in_path(args[0]);
+    if (command_path == NULL)
+    {
+        /* Command not found */
+        fprintf(stderr, "simple_shell: %s: command not found\n", args[0]);
+        return;
+    }
 
-	pid = fork();
-	if (pid == 0)
-	{
-		/* Child process */
-		if (execve(command_path, args, environ) == -1)
-		{
-			perror("simple_shell");
-		}
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0)
-	{
-		/* Error forking */
-		perror("simple_shell");
-	}
-	else
-	{
-		/* Parent process */
-		do {
-			waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
+    pid = fork();
+    if (pid == 0)
+    {
+        /* Child process */
+        if (execve(command_path, args, environ) == -1)
+        {
+            perror("simple_shell");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        /* Error forking */
+        perror("simple_shell");
+    }
+    else
+    {
+        /* Parent process */
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
 
-	free(command_path);
+    free(command_path);
 }
 
 /**
@@ -200,50 +211,50 @@ void execute_command(char **args)
  */
 char *find_command_in_path(char *command)
 {
-	char *path, *path_copy, *token, *full_path;
-	size_t command_len, dir_len;
+    char *path, *path_copy, *token, *full_path;
+    size_t command_len, dir_len;
 
-	path = getenv("PATH");
-	if (path == NULL)
-	{
-		return (NULL);
-	}
+    path = getenv("PATH");
+    if (path == NULL)
+    {
+        return (NULL);
+    }
 
-	path_copy = strdup(path);
-	if (path_copy == NULL)
-	{
-		perror("simple_shell");
-		exit(EXIT_FAILURE);
-	}
+    path_copy = strdup(path);
+    if (path_copy == NULL)
+    {
+        perror("simple_shell");
+        exit(EXIT_FAILURE);
+    }
 
-	command_len = strlen(command);
-	token = strtok(path_copy, ":");
-	while (token != NULL)
-	{
-		dir_len = strlen(token);
-		full_path = malloc(dir_len + command_len + 2); /* +2 for '/' and '\0' */
-		if (full_path == NULL)
-		{
-			free(path_copy);
-			perror("simple_shell");
-			exit(EXIT_FAILURE);
-		}
+    command_len = strlen(command);
+    token = strtok(path_copy, ":");
+    while (token != NULL)
+    {
+        dir_len = strlen(token);
+        full_path = malloc(dir_len + command_len + 2); /* +2 for '/' and '\0' */
+        if (full_path == NULL)
+        {
+            free(path_copy);
+            perror("simple_shell");
+            exit(EXIT_FAILURE);
+        }
 
-		strcpy(full_path, token);
-		strcat(full_path, "/");
-		strcat(full_path, command);
+        strcpy(full_path, token);
+        strcat(full_path, "/");
+        strcat(full_path, command);
 
-		if (access(full_path, X_OK) == 0)
-		{
-			free(path_copy);
-			return (full_path);
-		}
+        if (access(full_path, X_OK) == 0)
+        {
+            free(path_copy);
+            return (full_path);
+        }
 
-		free(full_path);
-		token = strtok(NULL, ":");
-	}
-	free(path_copy);
-	return (NULL);
+        free(full_path);
+        token = strtok(NULL, ":");
+    }
+    free(path_copy);
+    return (NULL);
 }
 
 /**
@@ -251,12 +262,11 @@ char *find_command_in_path(char *command)
  */
 void print_env(void)
 {
-	char **env = environ;
+    char **env = environ;
 
-	while (*env)
-	{
-		printf("%s\n", *env);
-		env++;
-	}
+    while (*env)
+    {
+        printf("%s\n", *env);
+        env++;
+   }
 }
-
